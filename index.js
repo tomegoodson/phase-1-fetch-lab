a8175fd542df3ee8fa3c35b6224374dcb8fe1144
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-14 17:22:46 -0500

    fetch.md

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  const url = 'https://anapioficeandfire.com/api/books';
 
+  return fetch(url)
+    .then(response => response.json()) 
+    .then(data => renderBooks(data)) 
+    .catch(error => console.error('Error fetching books:', error)); 
+}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.name; 
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
